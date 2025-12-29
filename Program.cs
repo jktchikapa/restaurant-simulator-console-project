6976d30361db66c4a094c93b06961964ef6ca4c5
jktchikapa <jktchikapa@gmail.com>
2025-12-29 11:42:47 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace restaurant_simulator_console_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
